a804fddbde41a7dfa32a9b65d70856a45500652a
Vitaliy_Stepanenko <utihar30@gmail.com>
2023-05-18 09:39:06 +0300

    update feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Statistics from "./Statistics";
 import FeedbackOptions from "./FeedbackOptions";
 import Section from "./Section";
+import { useState } from "react";
 
 
 // export const App = () => {
@@ -12,46 +13,42 @@ import Section from "./Section";
 //   );
 // };
 
-export class App extends React.Component {
-     state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-
-  };
-
- handleGood = () => {
-    this.setState(prevState => ({
-        good: prevState.good +1,
-    }))};
-  handleNeutral = () => {
-     this.setState((prevState) => {
-      return {
-          neutral: prevState.neutral +1,
-      }
-    })};
-  handleBad = () => {
-     this.setState((prevState) => {
-      return {
-           bad: prevState.bad +1,
-      }
-     })
-  };
-
-  render() {
+export const App = ()=>  {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
   
+  //    state = {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+
+  // };
+
+ const handleGood = () => {
+    setGood((prevState) => 
+         prevState +1,
+    )};
+  const handleNeutral = () => {
+     setNeutral((prevState) => 
+      
+           prevState +1,
+    )};
+ const handleBad = () => {
+     setBad((prevState) => 
+           prevState +1,
+     )
+  };
     return (<div>
       <Section title="Please leave feedback"  >
-      <FeedbackOptions handleGood={this.handleGood} handleNeutral={this.handleNeutral} handleBad={this.handleBad} />
+      <FeedbackOptions handleGood={handleGood} handleNeutral={handleNeutral} handleBad={handleBad} />
       </Section>
      
       <Section title="Statistics"  >
-        <Statistics good={this.state.good} neutral ={this.state.neutral} bad={this.state.bad} />
+        <Statistics good={good} neutral ={neutral} bad={bad} />
       </Section>
       
     </div>
     
 )
-
-  }
 }
